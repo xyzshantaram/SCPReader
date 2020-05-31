133350aca35062bc1f6c8c3782685cf8c07e55c5
simsid66 <simsid66@gmail.com>
2020-06-01 02:42:23 +0530

    added some error handling

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 
 npm install
 rm scpread
+echo
 echo -e "#!/usr/bin/env bash" >> scpread
-echo -e "node \"$PWD/main.js\" \"\$1\" | less" >> scpread
+echo -e "if [[ \$1 =~ ^[0-9]+$ ]]; then " >> scpread
+echo -e "node \"$PWD/main.js\" \"\$1\" | less; else" >> scpread
+echo -e "echo -n \"Please enter a valid numerical SCP code.\"; fi" >> scpread
 echo -ne "Select directory to install SCPReader to. (example: /home/user/bin) (preferably, this should be included in your \$PATH): "; read -r scp_install_dir
 cp scp "$scp_install_dir"/scpread
 chmod +x "$scp_install_dir"/scpread
 sleep 1
-echo -e "Install completed. Hopefully nothing broke. Enjoy stealthy SCP reading! Please do not delete this directory until such time you wish to uninstall."\
+echo
+echo -e "Install completed. Hopefully nothing broke. Enjoy stealthy SCP reading!\nPlease do not delete this directory until such time you wish to uninstall."\
         "\nTo uninstall, simply issue 'rm "$scp_install_dir"/scpread' and delete this directory."
